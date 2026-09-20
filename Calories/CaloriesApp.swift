f38f2bc7845b32b4c7e96d52b4fe9607e2b1d15d
radblesk <144258807+radblesk@users.noreply.github.com>
2026-09-20 18:19:41 +0200

    Initial Commit

---
+++ Calories/CaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesApp.swift
+//  Calories
+//
+//  Created by Radoslav Bley on 20/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
