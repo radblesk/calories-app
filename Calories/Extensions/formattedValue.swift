89265bcb28f0a5dcebbe9bdaa6dbb44e3b4bbace
radblesk <144258807+radblesk@users.noreply.github.com>
2026-09-23 12:28:58 +0200

    feat(extensions): improved formattedValue method

--- Calories/Extensions/formattedValue.swift
+++ Calories/Extensions/formattedValue.swift
@@ -9,26 +9,32 @@ internal import HealthKit
 import SwiftUI
 
 extension HKQuantitySample {
-    func formattedValue(fractionLength: Int = 0) -> String {
+    func formattedValue(fractionLength: Int = 0, grouping: NumberFormatStyleConfiguration.Grouping = .automatic) -> String {
         let unit =
             UserDefaults.standard.string(forKey: "unit")
             .flatMap(Unit.init(rawValue:)) ?? .kcal
-        return quantity.doubleValue(for: unit.hkUnit).formatted(.number.precision(.fractionLength(fractionLength)))
+        return quantity.doubleValue(for: unit.hkUnit).formatted(.number.precision(.fractionLength(0...fractionLength)).grouping(grouping))
     }
 }
 
 extension HKStatistics {
-    func formattedValue(fractionLength: Int = 0) -> String {
+    func formattedValue(fractionLength: Int = 0, grouping: NumberFormatStyleConfiguration.Grouping = .automatic) -> String {
         let unit =
             UserDefaults.standard.string(forKey: "unit")
             .flatMap(Unit.init(rawValue:)) ?? .kcal
         let value = sumQuantity()?.doubleValue(for: unit.hkUnit) ?? 0
-        return value.formatted(.number.precision(.fractionLength(fractionLength)))
+        return value.formatted(.number.precision(.fractionLength(0...fractionLength)).grouping(grouping))
     }
     func extractedValue() -> Double {
         let unit =
             UserDefaults.standard.string(forKey: "unit")
             .flatMap(Unit.init(rawValue:)) ?? .kcal
         return sumQuantity()?.doubleValue(for: unit.hkUnit) ?? 0
+    }
+}
+
+extension Double {
+    func formattedValue(fractionLength: Int = 0, grouping: NumberFormatStyleConfiguration.Grouping = .automatic) -> String {
+        return formatted(.number.precision(.fractionLength(0...fractionLength)).grouping(grouping))
     }
 }
